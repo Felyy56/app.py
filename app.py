bc5accb6be2213dee13ea7dd6a2d22ad7691b69b
Felyy56 <66552748+Felyy56@users.noreply.github.com>
2025-07-11 23:14:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Webhook mottagen:")
+    print(data)
+    return "OK", 200
